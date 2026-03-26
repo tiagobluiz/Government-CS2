eb98f5c7929b0f3d34ef64dba76ce127dc6c3eb7
Tiago Luiz <tiagobluiz.97@gmail.com>
2026-03-26 22:09:02 +0000

    Add Cities Skylines II mod template

---
+++ Mod.cs
@@ -0,0 +1,26 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace GovernmentCS2
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(GovernmentCS2)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
